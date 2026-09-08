6055a6ac3c553962ebe58e72fcccfa3d3d4b0700
ilku <ilyha1408@gmail.com>
2026-09-08 18:30:16 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Currency-converter/sw.js', { scope: '/Currency-converter/' })})}
